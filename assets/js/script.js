db163e0bdd813ca25e1f9ee562fa9c87ba2a55e8
krystalnickole1212 <krystalnickole_12@yahoo.com>
2022-03-19 18:43:24 -0500

    added lots of things

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,40 @@
-https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={API key} 
+
+
+var appId = "db627139d1df26bd369c66a7b0eca74b";
+var cityName;
+var searchButton = document.querySelector(".searchBtn");
+var cityInput = document.querySelector(".city");
+
+var getCoordinates = function () {
+   fetch ("http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appId=" + appId).then(function(response){
+      
+      return response.json();
+}).then(function(data){
+    var latitude = data[0].lat;
+    var longitude = data[0].lon;
+    return [latitude, longitude]; 
+})
+.then(function (value){
+    fetch(
+        "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=" + appId )
+
+        
+});
+};
+
+
+
+
+ 
+var citySearch = function (){
+
+    cityName = cityInput.value;
+    getCoordinates(cityName);
+
+};
+
+//set items to ls 
+//localStorage.setItem(cityInput, JSON.stringify(SearchButton));
+
+
+searchButton.addEventListener("click", citySearch );
